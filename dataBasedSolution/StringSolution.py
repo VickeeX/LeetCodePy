ff609f55487ada9848d3133c46336e8834759e83
VickeeX <727951723@qq.com>
2019-02-22 23:08:47 +0800

    87. Scramble String -- recursive

--- dataBasedSolution/StringSolution.py
+++ dataBasedSolution/StringSolution.py
@@ -590,3 +590,21 @@ class StringSolution:
                 count += 1
                 i += 1
         return s[start:end]
+
+    def isScramble(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        if len(s1) != len(s2) or sorted(s1) != sorted(s2):
+            return False
+        if s1 == s2:
+            return True
+        n = len(s1)
+        for i in range(1, n):
+            # swap the two part or not
+            if (self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:])) or (
+                        self.isScramble(s1[:i], s2[n - i:]) and self.isScramble(s1[i:], s2[:n - i])):
+                return True
+        return False
