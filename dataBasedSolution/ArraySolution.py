8b64812c003cad66e082dcb85245e5b73503fe23
VickeeX <727951723@qq.com>
2019-02-22 21:03:06 +0800

    85. Maximal Rectangle -- stack with O(n) time.

--- dataBasedSolution/ArraySolution.py
+++ dataBasedSolution/ArraySolution.py
@@ -1362,14 +1362,15 @@ class ArraySolution:
         :rtype: int
         """
         s, res, heights = [], 0, [0] + heights + [0]
+        print(heights)
         for i, height in enumerate(heights):
             if len(s) > 0:
                 while height < heights[s[-1]]:
-                    print(height, s[-1], heights[s[-1]])
                     top = s.pop()
+                    print(i, height, ": ", s[-1], heights[s[-1]], top)
                     res = max(res, heights[top] * (i - s[-1] - 1))
             s.append(i)
-            print(s)
+            # print(s)
         return res
 
     def largestRectangleAreaDP(self, heights):
@@ -1385,4 +1386,25 @@ class ArraySolution:
                 dp[j] = min(dp[j], heights[i + j])
                 ans = max(ans, dp[j] * (i + 1))
                 # print(dp)
+        return ans
+
+    def maximalRectangle(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix or not matrix[0]:
+            return 0
+        n = len(matrix[0])
+        height, ans = [0] * (n + 1), 0
+        for row in matrix:
+            for i in range(0, n):
+                height[i] = height[i] + 1 if row[i] == '1' else 0  # record the height of column
+            stack = [-1]
+            for i in range(0, n + 1):
+                while height[i] < height[stack[-1]]:  # compute the constant width
+                    h = height[stack.pop()]
+                    w = i - 1 - stack[-1]
+                    ans = max(ans, h * w)
+                stack.append(i)
         return ans
