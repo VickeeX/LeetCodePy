3ff55be7533e96ac042518b61c3e2e40b89d2920
VickeeX <727951723@qq.com>
2019-03-02 10:45:49 +0800

    153. Find Minimum in Rotated Sorted Array

--- dataBasedSolution/ArraySolution.py
+++ dataBasedSolution/ArraySolution.py
@@ -1567,3 +1567,21 @@ class ArraySolution:
             ans = max(ans, m1, m2)
         return ans
 
+    def findMin(self, nums: list) -> int:
+        if nums[0] <= nums[-1]:
+            return nums[0]
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = l + (r - l) // 2
+            if nums[mid] > nums[mid + 1]:
+                return nums[mid + 1]
+            elif nums[mid] < nums[mid - 1]:
+                return nums[mid]
+            elif nums[0] < nums[mid]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return -1
+        # for i in range(len(nums)):
+        #     if nums[i] < nums[i - 1] and nums[i] < nums[i + 1 if i != len(nums) - 1 else 0]:
+        #         return nums[i]
