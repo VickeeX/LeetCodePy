21bb3bc4005bccb1e53611ae124326f0380fc6a5
VickeeX <727951723@qq.com>
2019-03-13 10:39:55 +0800

    229. Majority Element II

--- dataBasedSolution/ArraySolution.py
+++ dataBasedSolution/ArraySolution.py
@@ -1818,3 +1818,19 @@ class ArraySolution:
                     ans.append(str(nums[start]) + "->" + str(n))
                 start = i + 1
         return ans
+
+    def majorityElement(self, nums: list) -> list:
+        # Boyer Moore majority algorithm
+        count1, count2, candidate1, candidate2 = 0, 0, 0, 1
+        for n in nums:
+            if n == candidate1:
+                count1 += 1
+            elif n == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                count1, candidate1 = 1, n
+            elif count2 == 0:
+                count2, candidate2 = 1, n
+            else:  # not the candidates
+                count1, count2 = count1 - 1, count2 - 1
+        return [n for n in (candidate1, candidate2) if nums.count(n) > len(nums) // 3]
