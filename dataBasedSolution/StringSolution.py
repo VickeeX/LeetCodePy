221567cb9ed0f0ebcb39d8e8548697915dc87db3
Wei Xing <727951723@qq.com>
2019-07-22 19:55:18 +0800

    468. Validate IP Address

--- dataBasedSolution/StringSolution.py
+++ dataBasedSolution/StringSolution.py
@@ -1132,3 +1132,20 @@ class StringSolution:
         counter = Counter(s)
         arr = sorted([(-counter[k], k) for k in counter.keys()]) # first sort by -counter[k], then character sort
         return ''.join([c * (-n) for n, c in arr])
+    
+    def validIPAddress(self, IP: str) -> str:
+        def IPV4Part(s):
+            try: return str(int(s))==s and 0<=int(s)<=255
+            except: return False
+        
+        def IPV6Part(s):
+            if len(s)>4: return False
+            try: return int(s, 16) >= 0 and s[0] != '-'
+            except: return False
+        
+        if IP.count('.')==3 and all(IPV4Part(s) for s in IP.split(".")):
+            return "IPv4"
+        if IP.count(':')==7 and all(IPV6Part(s) for s in IP.split(":")):
+            return "IPv6"
+        return "Neither"
+
