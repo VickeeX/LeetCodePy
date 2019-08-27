b66d93ceac5974e938caf03e2c2edb7f1ff81074
vickeex <727951723@qq.com>
2019-08-27 19:59:39 +0800

    516. Longest Palindromic Subsequence

--- dataBasedSolution/StringSolution.py
+++ dataBasedSolution/StringSolution.py
@@ -1337,3 +1337,18 @@ class StringSolution:
             return ans
 
         return helper(0, 0) + n
+
+    def longestPalindromeSubseq(self, s: str) -> int:
+        if not s:
+            return 0
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                if i == j:
+                    dp[i][j] = 1
+                elif s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1] + 2
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+        return dp[0][-1]
