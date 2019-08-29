b3466dd1d5bed4a1a85b7a7dfd06a4b5f62d2ecd
vickeex <727951723@qq.com>
2019-08-29 23:00:10 +0800

    519. Random Flip Matrix

--- desighClass/RandomizedSetDuplicate.py
+++ desighClass/RandomizedSetDuplicate.py
@@ -6,19 +6,54 @@
     Description  :    {TODO}
     Author       :    VickeeX
 """
-import random,collections
+import random, collections
+
+
+class RandomizedSet:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.nums, self.pos = [], {}
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val not in self.pos:
+            self.nums.append(val)
+            self.pos[val] = len(self.nums) - 1
+            return True
+        return False
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val in self.pos:
+            idx, last = self.pos[val], self.nums[-1]
+            self.nums[idx], self.pos[last] = last, idx
+            self.nums.pop()
+            del self.pos[val]
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        return self.nums[random.randint(0, len(self.nums) - 1)]
+
 
 class RandomizedCollection(object):
 
     def __init__(self):
         self.vals, self.idxs = [], collections.defaultdict(set)
 
-
     def insert(self, val):
         self.vals.append(val)
         self.idxs[val].add(len(self.vals) - 1)
         return len(self.idxs[val]) == 1
-
 
     def remove(self, val):
         if self.idxs[val]:
@@ -35,10 +70,39 @@ class RandomizedCollection(object):
         return random.choice(self.vals)
 
 
+class RandoPickIndexSolution:
+
+    #     def __init__(self, nums: List[int]):
+    #         self.dict = defaultdict(list)
+    #         for i,n in enumerate(nums):
+    #             self.dict[n].append(i)
+
+    #     def pick(self, target: int) -> int:
+    #         idx = random.randint(0, len(self.dict[target])-1)
+    #         return self.dict[target][idx]
+
+    def __init__(self, nums: list):
+        self.nums = nums
+
+    def pick(self, target: int) -> int:
+        return random.choice([i for i, n in enumerate(self.nums) if n == target])
 
 
-        # Your RandomizedSet object will be instantiated and called as such:
-        # obj = RandomizedSet()
-        # param_1 = obj.insert(val)
-        # param_2 = obj.remove(val)
-        # param_3 = obj.getRandom()
+class RandomFlipMatrixSolution:
+    # 519. Random Flip Matrix
+    def __init__(self, n_rows: int, n_cols: int):
+        self.rows, self.cows = n_rows, n_cols
+        self.last = n_rows * n_cols
+        self.rec = {}
+        # keep the matching of num to pos index
+
+    def flip(self) -> list:
+        choice = random.randint(0, self.last - 1)
+        index = self.rec.get(choice, choice)  # if choice is the last and modified before
+        self.last -= 1
+        self.rec[choice] = self.rec.get(self.last, self.last)  # modify choice's matching index
+        return list(divmod(index, self.cows))
+
+    def reset(self) -> None:
+        self.last = self.rows * self.cows
+        self.rec = {}
