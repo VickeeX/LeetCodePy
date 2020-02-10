3a298eb3595f723dddac91f0e4b8f4ee73e9c93a
vickeex <727951723@qq.com>
2020-02-10 19:55:19 +0800

    sort algorithm: insertion/shell/merge sort

--- collections_for_interview/SortingAlgorithm.py
+++ collections_for_interview/SortingAlgorithm.py
@@ -66,3 +66,71 @@ def selectSort(arr, tag):
         return minSelectSort(arr)
     else:
         return maxSelectSort(arr)
+
+
+def insertionSort(arr):
+    """
+    Time complexity:
+        avg: O(n^2); best: O(n); worst: O(n^2)
+    Space complexity:
+        O(1)
+    表现稳定，适合数据规模较小的情况
+    """
+    if len(arr) == 0:
+        return arr
+    for i in range(len(arr) - 1):
+        tmp, j = arr[i + 1], i
+        while j >= 0 and tmp < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = tmp
+    return arr
+
+
+def shellSort(arr):
+    """
+    Time complexity:
+        avg: O(nlgn); best: O(nlgn); worst: O(nlgn)
+    Space complexity:
+        O(1)
+    缩小增量的插入排序
+    """
+    if len(arr) == 0:
+        return arr
+    gap = len(arr) // 2
+    while gap > 0:
+        for i in range(gap, len(arr)):
+            tmp, idx = arr[i], i - gap
+            while idx >= 0 and arr[idx] > tmp:
+                arr[idx + gap] = arr[idx]
+                idx -= gap
+            arr[idx + gap] = tmp
+        gap //= 2
+    return arr
+
+
+def mergeSort(arr):
+    """
+    Time complexity:
+        avg: O(nlgn); best: O(nlgn); worst: O(nlgn)
+    Space complexity:
+        O(n)
+    """
+    if len(arr) < 2:
+        return arr
+
+    def merge(left, right):
+        tmp, l, r, li, ri = [], len(left), len(right), 0, 0
+        for idx in range(l + r):
+            if li >= l or (li < l and ri < r and left[li] > right[ri]):
+                tmp.append(right[ri])
+                ri += 1
+            else:
+                tmp.append(left[li])
+                li += 1
+        return tmp
+
+    mid = len(arr) // 2
+    left_arr = mergeSort(arr[:mid])
+    right_arr = mergeSort(arr[mid:])
+    return merge(left_arr, right_arr)
