8c56bf31666f4ee91c22d0d3545cd97612764484
vickeex <727951723@qq.com>
2020-02-21 20:58:46 +0800

    Sword offer

--- collections_for_interview/SwordOffer.py
+++ collections_for_interview/SwordOffer.py
@@ -345,3 +345,355 @@ def topic_20_is_numberic(s):
 def topic_20_is_numberic_(s):
     import re
     return re.match(r"^[\+\-]?[0-9]*(\.[0-9]*)?([eE][\+\-]?[0-9]+)?$", s)
+
+
+def topic_21_reorder_array_by_parity(array):
+    if len(array) < 2:
+        return array
+    i, j = 0, len(array) - 1
+    while i < j:
+        if array[i] % 2 == 1:
+            i += 1
+        else:
+            array[i], array[j] = array[j], array[i]
+            j -= 1
+    return array
+
+
+def topic_21_reorder_array_by_parity_keep(array):
+    if len(array) < 2:
+        return array
+    i = 0
+    while i != len(array):
+        if array[i] % 2 == 1:
+            tmp = i
+            for j in range(i - 1, -1, -1):
+                if array[j] % 2 == 0:
+                    array[tmp], array[j] = array[j], array[tmp]
+                    tmp = j
+        i += 1
+    return array
+
+
+def topic_22_find_kth_to_tail(head, k):
+    # 1 2 3 4 5
+    if not head or k < 1:
+        return None
+    slow, faster = head, head
+    while k > 1 and faster.next:
+        faster, k = faster.next, k - 1
+    if k > 1:
+        return None
+    while faster.next:
+        slow, faster = slow.next, faster.next
+    return slow
+
+
+def topic_23_entry_node_of_loop(head):
+    if not head or not head.next:
+        return None
+    slow, fast, step = head, head.next, 1
+    while slow != fast:
+        if not fast.next or not fast.next.next:
+            return None
+        slow, fast, step = slow.next, fast.next.next, step + 1
+    # the linked list has a ring of length step
+
+    slow, fast = head, head
+    while step > 0:
+        fast = fast.next
+        step -= 1
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next
+    return slow
+
+
+def topic_24_revers_linked_list(head):
+    # 1—>2—>3—>4—>5—>6
+    # t1 t2
+    if not head or not head.next:
+        return head
+    pre, t1, t2, bak = None, head, head.next, None
+    while t2.next:
+        bak = t2.next
+        t1.next = pre
+        t2.next = t1
+        pre = t1
+        t1, t2 = t2, bak
+    t2.next = t1
+    return t2
+
+
+def topic_25_merge_sorted_linked_list(h1, h2):
+    t1, t2, ans = h1, h2, ListNode(-1)
+    tmp = ans
+    while t1 and t2:
+        if t1.val < t2.val:
+            tmp.next, t1 = t1, t1.next
+        else:
+            tmp.next, t2 = t2, t2.next
+        tmp = tmp.next
+    while t1:
+        tmp.next, t1 = t1, t1.next
+        tmp = tmp.next
+    while t2:
+        tmp.next, t2 = t2, t2.next
+        tmp = tmp.next
+    return ans.next
+
+
+def topic_26_has_sub_tree(t1, t2):
+    def check(n1, n2):
+        if not n2:
+            return True
+        if not n1 or n1.val != n2.val:
+            return False
+        if (n2.left and not check(n1.left, n2.left)) or (n2.right and not check(n1.right, n2.right)):
+            return False
+        return True
+
+    if not t2 or (not t1 and t2):  # bug: None should match None!
+        return False
+    if check(t1, t2) or (t1.left and topic_26_has_sub_tree(t1.left, t2)) or (
+            t1.right and topic_26_has_sub_tree(t1.right, t2)):
+        return True
+    return False
+
+
+def topic_27_mirror_of_binary_tree(root):
+    if not root:
+        return root
+    root.left, root.right = topic_27_mirror_of_binary_tree(root.right), topic_27_mirror_of_binary_tree(root.left)
+    return root
+
+
+def topic_28_is_symmetrical_binary_tree(root):
+    # # judgement of preorder (and rev_preorder) is incorrect: eg. {5,5,5,5,5,#,5}
+    # def preorder(node):
+    #     if not node:
+    #         return
+    #     preorder(node.left)
+    #     ans.append(node.val)
+    #     preorder(node.right)
+    #
+    # preorder(root)
+    # return ans == ans[::-1]
+
+    if not root:
+        return True
+    ans = [root]
+    while any(ans):
+        tmp = []
+        for x in ans:
+            if not x:
+                tmp.append(None)
+                tmp.append(None)
+            else:
+                tmp.append(x.left)
+                tmp.append(x.right)
+        vals = [n.val if n else '#' for n in tmp]
+        if vals != vals[::-1]:
+            return False
+        ans = tmp
+    return True
+
+
+def topic_29_print_matrix_clockwise(matrix):
+    if not matrix or not matrix[0]:
+        return []
+    h, w = len(matrix), len(matrix[0])
+    circle = (min(h, w) + 1) // 2
+
+    def print_circle(ans, c):
+        if w - 2 * c == 1:
+            ans += [matrix[i][c] for i in range(c, h - c)]
+        elif h - 2 * c == 1:
+            ans += [matrix[c][j] for j in range(c, w - c)]
+        else:
+            for j in range(c, w - c):
+                ans.append(matrix[c][j])
+            for i in range(c + 1, h - c):
+                ans.append(matrix[i][w - c - 1])
+            for j in range(w - c - 2, c - 1, -1):
+                ans.append(matrix[h - c - 1][j])
+            for i in range(h - c - 2, c, -1):
+                ans.append(matrix[i][c])
+        return ans
+
+    ans = []
+    for c in range(circle):
+        ans = print_circle(ans, c)
+    return ans
+
+
+class Topic30MinStack:
+    stack, minstack = [], []
+
+    def push(self, node):
+        self.stack.append(node)
+        if not self.minstack or node <= self.minstack[-1]:
+            self.minstack.append(node)
+
+    def pop(self):
+        if self.stack[-1] == self.minstack[-1]:
+            self.minstack.pop()
+        self.stack.pop()
+
+    def top(self):
+        return self.stack[-1] if self.stack else None
+
+    def min(self):
+        return self.minstack[-1] if self.minstack else None
+
+
+def topic_31_is_stack_pop_order(p1, p2):
+    stack, i, j = [], 0, 0
+    while i < len(p2) and j < len(p1):
+        stack.append(p1[j])
+        j += 1
+        while stack and stack[-1] == p2[i]:
+            stack.pop()
+            i += 1
+    if i == len(p2):
+        return True
+    return False
+
+
+def topic_32_print_tree_from_top_to_bottom(root):
+    if not root:
+        return []
+    layer, ans = [root], []
+    while layer:
+        tmp = []
+        for n in layer:
+            ans.append(n.val)
+            if n.left:
+                tmp.append(n.left)
+            if n.right:
+                tmp.append(n.right)
+        layer = tmp
+    return ans
+
+
+def topic_32_print_tree_from_top_to_bottom_reverse_line(root):
+    if not root:
+        return []
+    layer, ans = [root], []
+    while layer:
+        tmp = []
+        ans.append([])
+        for n in layer:
+            ans[-1].append(n.val)
+            if n.left:
+                tmp.append(n.left)
+            if n.right:
+                tmp.append(n.right)
+        layer = tmp
+    ans = [n if i % 2 == 0 else n[::-1] for i, n in enumerate(ans)]
+    return ans
+
+
+def topic_33_verify_postorder_squence_of_bst(sequence):
+    # [7,4,6,5]
+    if not sequence:
+        return False
+    if len(sequence) == 1:
+        return True
+    idx = -1
+    for i in range(len(sequence) - 1):
+        if sequence[i] > sequence[-1]:
+            idx = i
+            break
+    if idx == -1:
+        return topic_33_verify_postorder_squence_of_bst(sequence[:-1])
+    elif idx == 0:
+        return min(sequence[:-1]) > sequence[-1] and topic_33_verify_postorder_squence_of_bst(sequence[:-1])
+    else:
+        return min(sequence[idx:-1]) > sequence[-1] and \
+               topic_33_verify_postorder_squence_of_bst(sequence[:idx]) and \
+               topic_33_verify_postorder_squence_of_bst(sequence[idx:-1])
+
+
+def topic_34_find_target_path_in_tree(root, target):
+    # mark: path refers to "root to leaf"
+    ans = []
+
+    def helper(node, cur, left):
+        if not node or node.val > left:
+            return
+        if node.val == left and not node.left and not node.right:
+            ans.append(cur + [node.val])
+        elif node.val < left:
+            helper(node.left, cur + [node.val], left - node.val)
+            helper(node.right, cur + [node.val], left - node.val)
+
+    helper(root, [], target)
+    ans.sort(key=lambda x: -len(x))
+    return ans
+
+
+class RandomListNode:
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
+
+
+def topic_35_clone_complex_linked_list(head):
+    # time complexity: O(n)
+    # space complexity: O(n)
+    if not head:
+        return None
+    newh, ntmp, tmp = None, None, head
+    randoms, clones = {}, {}
+
+    while tmp:
+        if tmp == head:
+            newh = RandomListNode(head.label)
+            ntmp = newh
+            clones[head] = newh
+        else:
+            ntmp.next = RandomListNode(tmp.label)
+            ntmp = ntmp.next
+            clones[tmp] = ntmp
+        if tmp.random:
+            randoms[tmp] = tmp.random
+        tmp = tmp.next
+
+    for k, v in randoms.items():
+        clones[k].random = clones[v]
+    return newh
+
+
+def topic_35_clone_complex_linked_list_(head):
+    # NOT PASSED !!!
+    # time complexity: O(n)
+    # space complexity: O(1)
+    if not head:
+        return head
+    tmp = head
+    # step 1: replciate node to its next position
+    while tmp:
+        t = tmp.next
+        tmp.next = RandomListNode(tmp.label)
+        tmp.next.next = t
+        tmp = tmp.next.next
+
+    # step 2: replicate random
+    tmp = head
+    while tmp:
+        if tmp.random:
+            tmp.next.random = tmp.random.next
+        tmp = tmp.next.next
+
+    # step 3: split new nodes
+    newh = head.next
+    t1, t2 = head, newh
+    t1.next = None
+    while t2.next:
+        t1.next = t2.next
+        t2.next = t2.next.next
+        t1 = t1.next
+        t2 = t2.next
+    return newh
